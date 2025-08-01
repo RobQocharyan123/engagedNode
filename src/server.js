709a5aeb7c9507d0bc63ba4fe3fd88c7de668a97
RobQocharyan <robqocharyan@mail.ru>
2025-08-01 13:26:51 +0400

    bugfix

--- src/server.js
+++ src/server.js
@@ -12,18 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// gRr9lhBQdNqcLKzp  its password my db if its not work
-
 app.use("/api", router);
-// app.use('/api', router);
-// app.use('/api', router);
-// app.get('/remaining-time', (req, res) => {
-//   const now = Date.now();
-//   const elapsed = now - countdownStart;
-//   const remaining = Math.max(0, COUNTDOWN_DURATION - elapsed);
-
-//   res.json({ remaining_time_miliseconds: remaining });
-// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
